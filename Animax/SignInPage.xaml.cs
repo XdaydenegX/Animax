ab855f14473f8d1d5f773341bb72ae787e4eb400
XdaydenegX <yurcin026@gmail.com>
2023-11-30 15:51:59 +0700

    Добавьте файлы проекта.

---
+++ Animax/SignInPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animax
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Sign_Up_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            AuthWindow authwindow = (AuthWindow)Application.Current.MainWindow;
+            Frame mainframe = authwindow.MainFrame;
+            mainframe.Content = new SignUpPage().SignUpPageGrid;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string login_email_regex = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+            if (!string.IsNullOrEmpty(login_email_textbox.Text.ToString()) || Regex.IsMatch(login_email_textbox.Text, login_email_regex))
+            {
+                login_email_error_label.Visibility = Visibility.Hidden;
+                login_button.IsEnabled = true;
+            }
+            else
+            {
+                login_email_error_label.Visibility = Visibility.Visible;
+                login_button.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShopWindow shopWindow = new ShopWindow();
+            shopWindow.Show();
+            AuthWindow authwindow = (AuthWindow)Application.Current.MainWindow;
+            authwindow.Close();
+
+        }
+
+        private void login_password_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            login_password_error_label.Visibility = Visibility.Hidden;
+
+            if (login_password_textbox.Text != "" && login_password_textbox.Text.Length <= 20)
+            {
+                login_password_error_label.Visibility = Visibility.Visible;
+                login_password_error_label.Content = $"{login_password_textbox.Text.Length} / 20";
+                login_button.IsEnabled = true;
+                login_password_textbox.IsEnabled = true;
+            }
+            else
+            {
+                login_password_error_label.Content = "[ ! ]";
+                login_password_error_label.Visibility = Visibility.Visible;
+                login_button.IsEnabled = false;
+            }
+
+        }
+
+    }
+}
---
+++ Animax/SignUpPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animax
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpPage.xaml
+    /// </summary>
+    public partial class SignUpPage : Page
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Sign_In_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            AuthWindow authwindow = (AuthWindow)Application.Current.MainWindow;
+            Frame mainframe = authwindow.MainFrame;
+            mainframe.Content = new SignInPage().SignInPageGrid;
+        }
+
+        private void sign_up_email_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string sign_up_email_regex = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+            if (!string.IsNullOrEmpty(sign_up_email_textbox.Text.ToString()) || Regex.IsMatch(sign_up_email_textbox.Text, sign_up_email_regex))
+            {
+                sign_up_button.IsEnabled = true;
+            }
+            else
+            {
+                sign_up_button.IsEnabled = false;
+            }
+        }
+
+        private void sign_up_password_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sign_up_password_error_label.Visibility = Visibility.Hidden;
+
+            if (sign_up_password_textbox.Text != "" && sign_up_password_textbox.Text.Length <= 20 || sign_up_repeat_password_textbox.Text == sign_up_password_textbox.Text)
+            {
+                sign_up_password_error_label.Visibility = Visibility.Visible;
+                sign_up_password_error_label.Content = $"{sign_up_password_textbox.Text.Length} / 20";
+                sign_up_button.IsEnabled = true;
+                sign_up_password_textbox.IsEnabled = true;
+            }
+            else
+            {
+                sign_up_password_error_label.Content = "[ ! ]";
+                sign_up_password_error_label.Visibility = Visibility.Visible;
+                sign_up_button.IsEnabled = false;
+            }
+        }
+
+        private void sign_up_repeat_password_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sign_up_repeat_password_error_label.Visibility = Visibility.Hidden;
+
+            if (sign_up_repeat_password_textbox.Text != "" && sign_up_repeat_password_textbox.Text.Length <= 20 && sign_up_repeat_password_textbox.Text == sign_up_password_textbox.Text)
+            {
+                sign_up_repeat_password_error_label.Visibility = Visibility.Visible;
+                sign_up_repeat_password_error_label.Content = $"{sign_up_repeat_password_textbox.Text.Length} / 20";
+                sign_up_button.IsEnabled = true;
+                sign_up_repeat_password_textbox.IsEnabled = true;
+                sign_up_password_error_label.Visibility= Visibility.Hidden;
+            }
+            else
+            {
+                sign_up_password_error_label.Content = "[ ! ]";
+                sign_up_repeat_password_error_label.Visibility = Visibility.Visible;
+                sign_up_button.IsEnabled = false;
+            }
+        }
+    }
+}
